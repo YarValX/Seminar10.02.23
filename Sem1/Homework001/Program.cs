0b4f98214da70c49a951257d5ed6d6009b6268a9
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-15 09:53:21 +0500

    Program

--- Sem1/Homework001/Program.cs
+++ Sem1/Homework001/Program.cs
@@ -1,14 +1,14 @@
 ﻿Console.WriteLine("Введите первое число: ");
-int num_A = Convert.ToInt32(Console.ReadLine());
+int numA = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число: ");
-int num_B = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
 
-if (num_A > num_B)
+if (numA > numB)
 {
-    Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
+    Console.WriteLine("Первое число " + numA + " больше чем второе " + numB);
 }
 else
 {
-    Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
+    Console.WriteLine("Второе число " + numB + " больше чем первое " + numA);
 }
