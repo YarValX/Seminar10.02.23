ebe64d1e5f94067883590bd085e2467f3a202125
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-15 12:27:36 +0500

    Homework 005

--- Sem2/Homework005/Program.cs
+++ Sem2/Homework005/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
+int number = ReadInt("Введите трехзначное число: ");
+int amount = number.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
