1623e666a1827d3a926da726e08fe944eb59e3a2
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-15 09:54:11 +0500

    Program

--- Sem1/Homework002/Program.cs
+++ Sem1/Homework002/Program.cs
@@ -1,18 +1,18 @@
 ﻿Console.WriteLine("Введите 3 числа:");
-            int num_1 = Convert.ToInt32(Console.ReadLine());
-            int num_2 = Convert.ToInt32(Console.ReadLine());
-            int num_3 = Convert.ToInt32(Console.ReadLine());
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
 
-            int max = num_1;
+            int max = num1;
 
-            if (num_2 > max)
+            if (num2 > max)
             {
-                max = num_2;
+                max = num2;
             }
 
-            if (num_3 > max)
+            if (num3 > max)
             {
-                max = num_3;
+                max = num3;
             }
 
             Console.WriteLine("Наибольшее из введённых чисел -> " + max);
