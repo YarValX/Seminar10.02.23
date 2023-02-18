d29bee9786b20f6e9076b7471c3f159ad86dcf6c
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-18 09:31:29 +0500

    Вывести квадрат числа

--- FreeWork/Program.cs
+++ FreeWork/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int x;
+        int y;
+        
+        x = (int) inputValue();
+        y = (int) Math.Pow(x, 2);
+        Console.WriteLine(y);
+    }
+    
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
