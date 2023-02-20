d89399135e56e0a0de394c85389b733ce6b42de5
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-20 14:10:44 +0500

    Homework 008

--- Sem3/HomeWork008/Program.cs
+++ Sem3/HomeWork008/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToString(Console.ReadLine());
+}
+
+bool ispalindrome(string number)
+
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Это палиндром.");
+        return true;
+    }
+    else 
+    {
+        Console.WriteLine("Это не палиндром!");
+        return false;     
+    }
+}
+
+
+string number = ReadInt("Введите пятизначное число: ");
+
+bool result = ispalindrome(number);
