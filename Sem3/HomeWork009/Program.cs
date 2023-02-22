00972226efb7c567fa05ec2693e5f4387dfec4f2
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-22 11:24:50 +0500

    Home Work 009

---
+++ Sem3/HomeWork009/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Cube(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+    }
+}
+
+int N = ReadInt("Введите число: ");
+Cube(N);
