fb233e48ed5b9caedb8fd0dfa492a25cb5a99591
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-27 11:45:32 +0500

    Homework 014

---
+++ Sem5/Homework014/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[] myArray = new int[4];
+Random rand = new Random();
+for (int x = 0; x < myArray.Length; x++)
+      {
+          myArray[x] = rand.Next(100, 999);
+          Console.WriteLine(myArray[x]);
+      }
+
+int count = 0;
+
+for (int x = 0; x < myArray.Length; x++)
+{
+   if (myArray[x]%2==0)
+   count++;
+}
+Console.WriteLine($"из {myArray.Length} чисел, {count} четных");
+
+void PrintEven(int[] myArray)
+{
+    Console.Write("[ ");
+        for(int i = 0; i < myArray.Length; i++)
+        {
+            Console.Write(myArray[i] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine();
+}
+   
