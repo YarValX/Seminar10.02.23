0f238a359458dc9eeb04291e42bc8859584b8c32
YarValX <124351076+YarValX@users.noreply.github.com>
2023-02-27 14:39:12 +0500

    Homework 016

--- Sem5/Homework015/Program.cs
+++ Sem5/Homework015/Program.cs
@@ -2,14 +2,14 @@
 int sumElements = 0;
 Random rand = new Random();
 for (int x = 0; x < myArray.Length; x++)
-      {
-          myArray[x] = rand.Next(0, 99);
-          Console.WriteLine(myArray[x] + " ");
-      
-if (x % 2 == 1)
-      {
-        sumElements += myArray[x];
-      }
-    }
+{
+    myArray[x] = rand.Next(0, 99);
+    Console.WriteLine(myArray[x] + " ");
 
- Console.WriteLine($"всего из {myArray.Length} чисел, сумма элементов на нечётных позициях = {sumElements}");
+    if (x % 2 == 1)
+    {
+        sumElements += myArray[x];
+    }
+}
+
+Console.WriteLine($"всего из {myArray.Length} чисел, сумма элементов на нечётных позициях = {sumElements}");
---
+++ Sem5/Homework016/Program.cs
@@ -0,0 +1,22 @@
+﻿double[] myArray = new double[6];
+Random rand = new Random();
+for (int x = 0; x < myArray.Length; x++)
+      {
+          myArray[x] = rand.Next(1, 99);
+          Console.WriteLine(myArray[x] + "");
+      }
+double minArray = myArray[0];
+double maxArray = myArray[0];
+for (int x =1; x < myArray.Length; x++)
+{
+    if (minArray > myArray[x])
+    {
+        minArray = myArray[x];
+    }
+    if (maxArray < myArray[x])
+    {
+        maxArray = myArray[x];
+    }
+}
+double decision = maxArray - minArray;
+Console.WriteLine($"разница между между максимальным ({maxArray}) и минимальным({minArray}) элементами: {decision}");
