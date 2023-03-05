0adbffac8d2a7dd73587f18d3ba97f71e442ff83
YarValX <124351076+YarValX@users.noreply.github.com>
2023-03-05 15:25:38 +0500

    Homework020

---
+++ Sem7/Homework020/Program.cs
@@ -0,0 +1,42 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void PrintArray(int[,] matr)
+{
+for (int i=0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void ArrayValue(int[,] matr, int rows, int colums)
+{
+    if (rows < matr.GetLength(0) && colums < matr.GetLength(1)) Console.WriteLine(matr[rows, colums]);
+    else Console.WriteLine($"{rows},{colums} -> такого элемента в массиве нет");
+}
+
+
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца: ");
+int[,] matrix = new int [3, 4];  
+FillArray(matrix);
+PrintArray(matrix);
+ArrayValue(matrix,rows, colums);
+
