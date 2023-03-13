dc1972305696660ddaf90e539957260d71cf9943
YarValX <124351076+YarValX@users.noreply.github.com>
2023-03-13 15:01:03 +0500

    Homework026

---
+++ Sem9/Homework026/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int SumFor(int M, int N)
+{
+    int result = 0;
+    for (int i = M; i <= N; i++) result += i;
+    return result;
+}
+int SumRec(int M, int N)
+{
+    if (M > N) return 0;
+    if (M == N) return N;
+    else return M + SumRec(M + 1, N);
+}
+Console.WriteLine(SumFor(M, N));
+Console.WriteLine(SumRec(M, N));
