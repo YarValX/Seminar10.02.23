71ad61a31fbeb2d319131d9fc6da88c199cb10ed
YarValX <124351076+YarValX@users.noreply.github.com>
2023-03-13 10:34:58 +0500

    Homework025

---
+++ Sem9/Homework025/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+string NumbersFor (int N, int b)
+{
+    string result = String.Empty;
+    for (int i = N; i >= b; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec (int N, int b)
+{
+    if (N <= b) return NumbersRec(N + 1, b) + $"{N} ";
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersFor(N, b));
+Console.WriteLine(NumbersRec(N, b));
